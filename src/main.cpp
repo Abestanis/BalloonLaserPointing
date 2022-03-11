0568fad6eb42d51307fb356a919adfb742827501
Alexandre <alexandre.cappe@hotmail.fr>
2022-03-11 16:19:10 +0100

    Add transformation function between GPS coordinates and LTP coordinates

--- src/main.cpp
+++ src/main.cpp
@@ -30,6 +30,12 @@ static unsigned long lastMeasurementMillis = 0;
 static double targetBaseAngle = 0;
 /** The radius of the Earth in meter. */
 #define  Rt 6378.137e3
+/** Geometrical parameters for coordinates calculation in the LTP */
+double a = 6378137.0;
+double b= 6356752.3142;
+double f=(a-b)/a;
+double e= sqrt(f*(2-f));
+
 
 void setup() {
     // Initialize the serial port.
@@ -55,33 +61,62 @@ static double normalizeAngle(double angle) {
 
 
 struct coord_GPS {
+    /** Angles have to be transform in radian before assignation !!! **/
     double longitude;
     double latitude;
     double altitude;
 };
 
-coord_GPS laser = {
+coord_GPS laser_GPS = {
+        /** Angle in radian !! **/
         .longitude=23,
         .latitude=45,
         .altitude= 23,
 };
 
-struct coord_ECI_moved {
+
+struct coord_xyz {
     double x;
     double y;
     double z;
 };
 
-static coord_ECI_moved conversion_GPS_to_ECI_moved(coord_GPS balloon_GPS) {
-    coord_ECI_moved balloon = {
-            .x=Rt *
-               (cos(balloon_GPS.latitude) * cos(balloon_GPS.longitude) - cos(laser.latitude) * cos(laser.longitude)),
-            .y=Rt *
-               (cos(balloon_GPS.latitude) * sin(balloon_GPS.longitude) - cos(laser.latitude) * sin(laser.longitude)),
-            .z=Rt * (sin(balloon_GPS.latitude) - sin(laser.latitude)),
-    };
+float N_laser = a/ sqrt(1-pow(e,2)*pow(sin(laser_GPS.latitude),2));
 
-    return balloon;
+coord_xyz laser_ECEF{
+    .x=(laser_GPS.altitude+N_laser)*cos(laser_GPS.latitude)*cos(laser_GPS.longitude),
+    .y=(laser_GPS.altitude+N_laser)*cos(laser_GPS.latitude)*sin(laser_GPS.longitude),
+    .z=(laser_GPS.altitude+(1-pow(e,2))*N_laser)*sin(laser_GPS.latitude)
+};
+
+
+
+static coord_xyz conversion_GPS_to_LTP(coord_GPS balloon_GPS) {
+    /** This function convert the coordinates given by the GPS to the Local Tangent Place reference frame. The center of this reference frame being the laser.**/
+
+    float N = a/ sqrt(1-pow(e,2)*pow(sin(balloon_GPS.latitude),2));
+
+    float lat=balloon_GPS.latitude;
+    float lon=balloon_GPS.longitude;
+    float alt=balloon_GPS.altitude;
+    coord_xyz b_ECEF;
+    coord_xyz b_LTP;
+    coord_xyz Transition; /*This is a transition vector used for calculation*/
+
+    b_ECEF.x=(alt+N)*cos(lat)*cos(lon);
+    b_ECEF.y=(alt+N)*cos(lat)*sin(lon);
+    b_ECEF.z=(alt+(1-pow(e,2))*N)*sin(lat);
+
+    Transition.x=b_ECEF.x-laser_ECEF.x;
+    Transition.y=b_ECEF.y-laser_ECEF.y;
+    Transition.z=b_ECEF.z-laser_ECEF.z;
+
+    b_LTP.x=Transition.x*(-sin(lat))+Transition.y*cos(lon);
+    b_LTP.y=Transition.x*(-cos(lon)*sin(lat))+Transition.y*(-sin(lat)*sin(lon))+Transition.z*cos(lat);
+    b_LTP.z=Transition.x*cos(lat)*cos(lon)+Transition.y*cos(lat)*sin(lon)+Transition.z*sin(lat);
+
+    return b_LTP;
+
 };
 
 
