a9bab6ea6a1b87608b42fe961c472fb640c28654
Abestanis <abestanis.gc@gmail.com>
2022-03-15 16:53:45 +0100

    Allow exiting the controller

--- controller/controller.py
+++ controller/controller.py
@@ -2,6 +2,7 @@ import struct
 
 from enum import Enum
 from threading import Thread
+from contextlib import contextmanager
 
 from serial import Serial
 
@@ -29,20 +30,26 @@ class CommandSerializer:
 class Connection(Thread):
     def __init__(self):
         super().__init__(daemon=True)
-        self._connection = Serial(baudrate=9600)
+        self._connection = Serial(baudrate=9600, timeout=1)
         self._connection.set_buffer_size(rx_size=640000)
+        self._isOpen = False
 
+    @contextmanager
     def open(self):
         self._connection.setPort('COM18')
         self._connection.open()
+        self._isOpen = True
+        yield
+        self._isOpen = False
 
     def send(self, data):
         self._connection.write(data)
 
     def run(self):
-        while True:
+        while self._isOpen:
             data = self._connection.read(self._connection.inWaiting() or 1)
             print(data.decode('utf-8', errors='replace'), end='', flush=True)
+        self._connection.close()
 
 
 class Controller:
@@ -56,23 +63,27 @@ class Controller:
         self._connection = Connection()
 
     def run(self):
-        self._connection.open()
-        self._connection.start()
-        while True:
-            inputData = input()
-            arguments = inputData.split()
-            command = arguments.pop(0)
-            try:
-                commandId = CommandId[command.upper()]
-            except KeyError:
-                print('=' * 30 + f'\nInvalid command: "{command}"\n' + '=' * 30)
-                continue
-            try:
-                commandData = self._SERIALIZERS[commandId](*arguments)
-            except TypeError as error:
-                print('=' * 30 + f'\nInvalid argument(s): "{error}"\n' + '=' * 30)
-                continue
-            self._connection.send(commandData)
+        with self._connection.open():
+            self._connection.start()
+            while True:
+                inputData = input()
+                arguments = inputData.split()
+                command = arguments.pop(0)
+                if command == 'q' or command == 'quit':
+                    break
+                try:
+                    commandId = CommandId[command.upper()]
+                except KeyError:
+                    print('=' * 30 + f'\nInvalid command: "{command}"\n' + '=' * 30)
+                    continue
+                try:
+                    commandData = self._SERIALIZERS[commandId](*arguments)
+                except TypeError as error:
+                    print('=' * 30 + f'\nInvalid argument(s): "{error}"\n' + '=' * 30)
+                    continue
+                self._connection.send(commandData)
+        print('Exiting...')
+        self._connection.join()
 
 
 def main():
