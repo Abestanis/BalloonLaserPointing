40f3ba7531e880b186cdd29a8cf8ab861d653408
Abestanis <abestanis.gc@gmail.com>
2022-03-17 16:14:30 +0100

    Add a simple three-dimensional vector class

---
+++ include/Vec3.h
@@ -0,0 +1,35 @@
+#pragma once
+
+/**
+ * A simple vector class that holds three values.
+ *
+ * @tparam T The base type of the individual values.
+ */
+template<typename T>
+class Vec3 {
+public:
+    /**
+     * Construct a new vector.
+     *
+     * @param x The x component of the vector.
+     * @param y The y component of the vector.
+     * @param z The z component of the vector.
+     */
+    constexpr Vec3(T x, T y, T z) : x(x), y(y), z(z) {
+    }
+
+    /**
+     * The x component of the vector.
+     */
+    T x;
+
+    /**
+     * The y component of the vector.
+     */
+    T y;
+
+    /**
+     * The z component of the vector.
+     */
+    T z;
+};
